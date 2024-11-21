b8fee5038f9d1fc0cdc0bf96c6df96c37f5ac7ad
Adan <adanaltamira@gmail.com>
2024-11-20 23:23:08 -0800

    Update app to show video and add lock files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe 
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0, 
+          bottom: 0, 
+          right: 0, 
+          width: "100%", 
+          height: "100%", 
+          border: "none", 
+          margin: 0, 
+          padding: 0, 
+          overflow: "hidden", 
+          zIndex: 999999
+        }}
+        src="https://www.youtube.com/embed/VYOjWnS4cMY?si=1PEIhsRWePg9QWMM&amp;controls=0&amp;autoplay=1" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen>
+
+        </iframe>
     </div>
   );
 }
