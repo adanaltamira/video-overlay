897150774d180f7a2d3f94421cf8e80b25b49846
Adan <adanaltamira@gmail.com>
2024-11-29 22:56:48 -0800

    add emotion css in JS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import PropTypes from 'prop-types';
+import styled from "@emotion/styled";
 
 const PokemonRow = ({pokemon, onSelect}) => (
   <tr>
@@ -51,7 +51,29 @@ PokemonInfo.propTypes = {
     "Sp. Defense": PropTypes.number.isRequired,
     Speed: PropTypes.number.isRequired
   })
-}
+};
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const Container = styled.div`
+  margin: auto;
+  width: 800px;
+  padding-top: 1rem
+`;
+
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`;
+
+const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 70% 30%;
+  grid-column-gap: 1rem;
+`;
 
 function App() {
   const [filter, filterSet] = React.useState("");
@@ -65,21 +87,11 @@ function App() {
   }, [filter])
 
   return (
-    <div className="App" style={{
-      margin: "auto",
-      width: 800,
-      paddingTop: "1rem"
-    }}>
-      <h1>Pokemon Search</h1>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "70% 30%",
-          gridColumnGap: "1rem"
-        }}
-      >
+    <Container>
+      <Title>Pokemon Search</Title>
+      <TwoColumnLayout>
         <div>
-          <input 
+          <Input 
             value={filter} 
             onChange={(evt) => {
               filterSet(evt.target.value.toLowerCase());
@@ -103,8 +115,8 @@ function App() {
           </table>
         </div>
         {selectedItem && <PokemonInfo {...selectedItem} />}
-      </div>
-    </div>
+      </TwoColumnLayout>
+    </Container>
   );
 }
 
