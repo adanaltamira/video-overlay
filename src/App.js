7190708eb0902f288558024e13decb0d8d2f44b4
Adan <adanaltamira@gmail.com>
2024-12-02 11:43:02 -0800

    Add orange

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 // import logo from './logo.svg';
 import './App.css';
+import YouTube from 'react-youtube';
+
+const youTubeOpts = {
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 1,
+    controls: 0,
+  },
+};
+
+const onStateChangeFunc = (evt) => {
+  if (evt.data === 2)
+    console.log("onStateChangeFunc", evt);
+};
 
 function App() {
   return (
     <div className="App">
-      <iframe 
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0, 
-          bottom: 0, 
-          right: 0, 
-          width: "100%", 
-          height: "100%", 
-          border: "none", 
-          margin: 0, 
-          padding: 0, 
-          overflow: "hidden", 
-          zIndex: 999999
-        }}
-        src="https://www.youtube.com/embed/VYOjWnS4cMY?si=1PEIhsRWePg9QWMM&amp;controls=0&amp;autoplay=1" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        referrerpolicy="strict-origin-when-cross-origin" 
-        allowfullscreen>
-
-        </iframe>
+      <div className="orangeButton" style={{
+        display: "none"
+      }}>
+        <img src="//localhost:3000/video-overlay/orange.png" alt="orange" />
+      </div>
+      <YouTube
+        videoId={"VYOjWnS4cMY"}                  // defaults -> ''
+        // id={string}                       // defaults -> ''
+        className={"youTubeStyle"}                // defaults -> ''
+        iframeClassName={"youTubeStyle"}          // defaults -> ''
+        // style={string}                    // defaults -> {}
+        // title={string}                    // defaults -> ''
+        // loading={string}                  // defaults -> undefined
+        opts={youTubeOpts}                        // defaults -> {}
+        // onReady={func}                    // defaults -> noop
+        // onPlay={func}                     // defaults -> noop
+        // onPause={func}                    // defaults -> noop
+        // onEnd={func}                      // defaults -> noop
+        // onError={func}                    // defaults -> noop
+        onStateChange={onStateChangeFunc}              // defaults -> noop
+        // onPlaybackRateChange={func}       // defaults -> noop
+        // onPlaybackQualityChange={func}    // defaults -> noop
+      />
     </div>
   );
 }
